1a3ebaf0b83eaafc74eec7de8461d97065638401
somoneuGuh <124320369+somoneuGuh@users.noreply.github.com>
2023-08-30 14:03:35 -0500

    agregué una función para mi formulario que en caso de que haya menos de 3 caracteres enla tarea, no deje crearla

--- my-react-task-list/src/components/Header/index.jsx
+++ my-react-task-list/src/components/Header/index.jsx
@@ -5,21 +5,32 @@ import { useState } from "react";
 
 export function Header({ onAddTask }) {
   const [title, setTitle] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
 
+    if (title.length < 3) {
+      setErrorMessage("Title must be at least 3 characters long");
+      return;
+    }
+
     onAddTask(title);
-    setTitle('');
+    setTitle("");
+    setErrorMessage("");
   }
 
   function onChangeTitle(event) {
     setTitle(event.target.value);
+    if (errorMessage) {
+     
+      setErrorMessage("");
+    }
   }
 
   return (
     <header className={styles.header}>
-      <img src={Logo} />
+      <img src={Logo} alt="Logo" />
 
       <form onSubmit={handleSubmit} className={styles.newTaskForm}>
         <input
@@ -28,9 +39,14 @@ export function Header({ onAddTask }) {
           value={title}
           onChange={onChangeTitle}
         />
+        <div className={styles.errorMessage}>
+          {" "}
+          {errorMessage && (
+            <p className={styles.errorMessage}>{errorMessage}</p>
+          )}
+        </div>
         <button>
-          Create
-          <AiOutlinePlusCircle size={20} />
+          Create <AiOutlinePlusCircle size={20} />
         </button>
       </form>
     </header>
