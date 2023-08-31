7c516864252c4f7bb84b674734f81535d383a514
somoneuGuh <124320369+somoneuGuh@users.noreply.github.com>
2023-08-30 22:43:23 -0500

    agregué Chakra al index.jsx de la carpeta (Tasks) en components

--- my-react-task-list/src/components/Tasks/index.jsx
+++ my-react-task-list/src/components/Tasks/index.jsx
@@ -1,3 +1,4 @@
+import { Box, Flex, Text } from "@chakra-ui/react";
 import { Task } from "../Task";
 import styles from "./tasks.module.css";
 
@@ -6,24 +7,30 @@ export function Tasks({ tasks, onComplete, onDelete }) {
   const completedTasks = tasks.filter(task => task.isCompleted).length;
 
   return (
-    <section className={styles.tasks}>
-      <header className={styles.header}>
-        <div>
-          <p>Create tasks</p>
+    <Box className={styles.tasks} w="100%" maxW="736px" mx="auto" mt="90px" p="1rem">
+      <Flex className={styles.header} justify="space-between" mb="24px">
+        <Flex>
+          <Text color="#4ea8de" fontSize="14px" fontWeight="700">
+            Create tasks
+          </Text>
           <span>{tasksQuantity}</span>
-        </div>
+        </Flex>
 
-        <div>
-          <p className={styles.textPurple}>Completed</p>
-          <span>{completedTasks} of {tasksQuantity}</span>
-        </div>
-      </header>
+        <Flex>
+          <Text className={styles.textPurple} fontSize="14px" fontWeight="700">
+            Completed
+          </Text>
+          <span>
+            {completedTasks} of {tasksQuantity}
+          </span>
+        </Flex>
+      </Flex>
 
-      <div className={styles.list}>
-        {tasks.map((tasks) => (
-          <Task key={tasks.id} task={tasks} onComplete={onComplete} onDelete={onDelete}/>
+      <Flex className={styles.list} flexDirection="column" gap="12px">
+        {tasks.map(task => (
+          <Task key={task.id} task={task} onComplete={onComplete} onDelete={onDelete} />
         ))}
-      </div>
-    </section>
+      </Flex>
+    </Box>
   );
 }
